b306da08efa15e7c636d8fb4b51a6c85f9bf84f5
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2023-05-19 13:52:56 +0700

    add HTTP communication_14

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -49,9 +49,10 @@ export class DetailsComponent {
   housingLocation: HousingLocation | undefined;
   housingLocationId = -1;
   constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation =
-      this.housingService.getHousingLocationById(housingLocationId);
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+      this.housingLocation = housingLocation;
+    });
   }
 
   applyForm = new FormGroup({
