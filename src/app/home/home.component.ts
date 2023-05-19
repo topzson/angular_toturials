b9e5a30d1bf656ef6853d44274668dc10623d4dd
topzson <76682932+topzson@users.noreply.github.com>
2023-05-19 09:29:07 +0700

    1 file modified, Ended Add a property binding to an component's_06

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,7 +16,7 @@ import { HousingLocation } from '../housinglocation';
     </section>
 
     <section class="results">
-      <app-housing-location></app-housing-location>
+      <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css'],
